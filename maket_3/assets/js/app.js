98c195416fdfdc53dc28887f289bedc4bf9002ff
Sergei <you@example.com>
2021-07-21 11:47:26 +0300

    first commit

---
+++ maket_3/assets/js/app.js
@@ -0,0 +1,11 @@
+// покраска header
+//let test = document.querySelector('header');
+//test.style.background="red";
+
+/*let div = document.querySelector('.header-index__button')
+div.addEventListener('click',mouseclick);
+function mouseclick(event){
+    window.scrollTo(0, 200)
+    console.log("Hello");
+}*/
+"use strict";
